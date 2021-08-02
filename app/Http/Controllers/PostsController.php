99ae348c2bb379cac27666f795a46e0a16a8c8e3
amrfatouh <66204477+amrfatouh@users.noreply.github.com>
2021-08-02 12:52:52 +0200

    10 Hello Eloquent

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -2,14 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Post;
 use Illuminate\Http\Request;
 
 class PostsController extends Controller
 {
-  // connect to the database and get a post
+  // use Post model
   public function show($slug)
   {
-    $post = \DB::table('posts')->where('slug', $slug)->first();
+    // use firstOrFail to get 404 if no posts were found
+    $post = Post::where('slug', $slug)->firstOrFail();
     return view('newpage', ['post' => $post]);
   }
 }
