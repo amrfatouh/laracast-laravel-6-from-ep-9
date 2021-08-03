4b613f8dc905690e2443ff12e731b6a483e12b67
amrfatouh <66204477+amrfatouh@users.noreply.github.com>
2021-08-03 11:24:24 +0200

    29 Basic Eloquent Relationships

--- app/User.php
+++ app/User.php
@@ -36,4 +36,13 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+  public function articles()
+  {
+    return $this->hasMany(Article::class); //select * from articles where user_id = $this->id
+  }
+  public function projects()
+  {
+    return $this->hasMany(Project::class); //select * from projects where user_id = $this->id
+  }
 }
